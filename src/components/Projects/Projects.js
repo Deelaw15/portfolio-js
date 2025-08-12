bebbaff933c49f803284e558dc7361fdff5165c5
Deelaw15 <lawalfaruq50@gmail.com>
2025-08-12 18:04:15 +0100

    commit

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,27 +1,34 @@
 import React from "react";
 import "./Projects.scss";
 import { bigProjects } from "../../portfolio";
-import Button from "../../components/button/Button";
+// import Button from "../../components/button/Button";
 // ...existing imports...
 
 export default function Projects() {
   const more = bigProjects?.moreProjects;
 
+  console.log("MORE PROJECTS CONFIG:", more);
+
   return (
     <div className="main" id="Featured projects">
       <div>
-        <h1 className="skills-heading">{bigProjects.title}</h1>
-        <p className="projects-subtitle">{bigProjects.subtitle}</p>
-        <div className="projects-container">{/* ...existing project cards... */}</div>
+        {bigProjects?.title && <h1 className="skills-heading">{bigProjects.title}</h1>}
+        {bigProjects?.subtitle && <p className="projects-subtitle">{bigProjects.subtitle}</p>}
+        
+        <div className="projects-container">
+          {/* ...existing project cards... */}
+        </div>
 
         {more?.link && (
           <div className="more-projects-container">
-            <Button
+            <a
               text={bigProjects.moreProjects.buttonText}
               href={bigProjects.moreProjects.link}
               newTab={true}
               className="project-button"
-            />
+            >
+              {more.buttonText || "View More Projects"}
+            </a>
           </div>
         )}
       </div>
